18be31be46f20f778a012c447244d2c6b48ec6b8
adriangpc <adriangpc@gmail.com>
2018-02-08 16:15:25 +0800

    After Template Inheritance

--- restaurants/views.py
+++ restaurants/views.py
@@ -1,3 +1,4 @@
+from random import randint
 from django.http import HttpResponse
 from django.shortcuts import render
 
@@ -17,4 +18,25 @@ from django.shortcuts import render
 #     return HttpResponse(html_)
 
 def home(request):
-    return render(request, "base.html", {"html_var": "Insert text here"})
+    num = None
+    some_list = [
+        randint(1, 1000000), 
+        randint(1, 1000000), 
+        randint(1, 1000000)
+        ]
+    cond_bool_item = False
+    if cond_bool_item:
+        num = randint(1,20000000)
+    context =  {
+        "num": num,
+        "some_list": some_list
+    }
+    return render(request, "home.html", context)
+
+def about(request):
+    context =  {}
+    return render(request, "about.html", context)
+
+def contact(request):
+    context =  {}
+    return render(request, "contact.html", context)
